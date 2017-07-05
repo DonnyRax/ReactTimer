ada4c93e5f9db9543fc49dce1c0b10ce9d76ce4f
Rakesh Chotalia <rakesh.chotalia@gmail.com>
2017-07-05 21:14:09 +0100

    countdown form added

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,9 @@ var Navigation = require('Navigation');
 var Main = (props) => {
     return (
         <div>
-            <div>
-                <Navigation/>
-                Main.jsx rendered
-                <div>
+            <Navigation/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}    
                 </div>
             </div>
